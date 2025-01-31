3c3fff3a628edfc4ab3973a2697da8b0e54f6343
danilohumberto” <danilohumberto12@hotmail.com>
2025-01-31 12:52:09 -0300

    continuing responsive feature

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -60,6 +60,38 @@ const Project = () => {
             </div>
           </div>
         </SwiperSlide>
+        <SwiperSlide className="swiper-box">
+          <div className="p-box">
+            <img src="./pokedex.png" alt="" />
+            <div className="p-box-content">
+              <div className="p-box-header">
+                <h3>{t("project.box.header.title")}</h3>
+                <div className="p-box-header-imgs">
+                  <img src="./html.png" alt="logo do html" />
+                  <img src="./css-3.png" alt="logo do css" />
+                  <img src="./js.png" alt="logo do javascript" />
+                </div>
+              </div>
+              <p>{t("project.box.box-content.paragraph")}</p>
+              <div className="p-box-content-btns">
+                <a
+                  href="https://danilo-humberto.github.io/ProjectPokedex/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {t("project.box.box-content.first-a")}
+                </a>
+                <a
+                  href="https://github.com/danilo-humberto/ProjectPokedex?tab=readme-ov-file"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  &lt;{t("project.box.box-content.second-a")} /&gt;
+                </a>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </section>
   );
