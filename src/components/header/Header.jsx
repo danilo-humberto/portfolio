c7922bf965af07dcc421dc521bb5b1ac4d29ad67
danilohumberto” <danilohumberto12@hotmail.com>
2025-02-10 20:12:22 -0300

    fix: fixing the menu so that when it is clicked outside, it also closes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,24 +3,45 @@ import './Header.css'
 import SwitchTheme from "../switchTheme/SwitchTheme";
 import SwitchLanguage from "../switchLanguage/SwitchLanguage";
 import HamburgerMenu from "../hamburgerMenu/HamburgerMenu";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
   const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
 
-  const handleOpenMenu = () => {
+  const handleOpenMenu = (event) => {
+    event.stopPropagation();
     setIsOpen(!isOpen)
   }
 
   const handleNavClick = () => {
     setIsOpen(false)
   }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      document.removeEventListener('click', handleClickOutside);
+    }
+
+    // Limpeza do evento ao desmontar ou fechar o menu
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen]);
   return (
     <header>
       <div className="container-header">
         <span>D</span>
-        <nav className={isOpen ? 'open' : ''} onClick={handleNavClick}>
+        <nav ref={menuRef} className={isOpen ? 'open' : ''} onClick={handleNavClick}>
           <ul className={isOpen ? 'open' : ''}>
             <li><a href="#home" onClick={handleNavClick}>{t("nav.home")}</a></li>
             <li><a href="#about" onClick={handleNavClick}>{t("nav.about")}</a></li>
